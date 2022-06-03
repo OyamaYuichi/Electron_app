75fb12672114db2f0321f6f23acc3a0bf3b75802
OyamaYuichi <o-yuichi@arsaga.jp>
2022-06-03 18:04:20 +0900

    BrowserViewについて

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-const { app, BrowserWindow } = require('electron');
-
-const html = '<html><head>'
-  + '<title>HTML STRING</title>'
-  + '</head><body>'
-  + '<h1>HTML STRING</h1>'
-  + '<p>This is string content.</p>'
-  + '</body></html>';
+const { app, BrowserView, BrowserWindow } = require('electron');
 
 function createWindow () {
   let win = new BrowserWindow( {
-    width: 400,
-    height: 250
+    width: 600,
+    height: 400
   });
-  let webc = win.webContents;
-  webc.on('new-window', ()=>{
-    console.log('new-window.');
-  });
-  webc.on('did-finish-load', ()=>{
-    console.log('did-finish-load.');
-  });
-  webc.on('dom-ready', ()=>{
-    console.log('dom-ready.');
-  });
-  webc.on('will-navigate', ()=>{
-    console.log('will-navigate.');
-  });
-  webc.on('did-navigate', ()=>{
-    console.log('did-navigate.');
-  });
-
   win.loadFile('index.html');
+
+  const view = new BrowserView();
+  view.webContents.loadURL('https://google.com');
+
+  win.setBrowserView(view);
+  view.setBounds({ x: 200, y: 150, width: 300, height: 150 });
 }
 
 app.whenReady().then(createWindow);
